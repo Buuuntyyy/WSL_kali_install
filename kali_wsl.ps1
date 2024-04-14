8bac3322aad7f3f1a3d1d071d1dbbcad9e5b7f8d
Bunnntyyy <104865101+Buuuntyyy@users.noreply.github.com>
2024-04-14 11:56:04 +0200

    Add files via upload
    Powershell script to install Kali-Linuw WSL

---
+++ kali_wsl.ps1
@@ -0,0 +1,18 @@
+﻿#create a Kali-Linux WSL in a second
+
+$version = $wslVersion = wsl --version | Select-String "WSL version" | ForEach-Object { $_.ToString().Split(':')[1].Trim() }
+if ($version -ne 2){
+    #activation des fonctionnalités de virtualisation
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+    Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+    Restart-Computer -Force
+
+}
+
+
+#Mise à jour de WSL
+wsl --update
+
+#Installation de Kali-linux
+wsl --install --distribution kali-linux
